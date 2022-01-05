1a4567ef8791b0cfee714bc3325804041768f12e
Nicandro <nikpote@hotmail.it>
2022-01-05 13:32:59 +0100

    room label, ready for merge

--- ClientAndroid/app/src/main/java/com/example/wifisignal/PlayerActivity.java
+++ ClientAndroid/app/src/main/java/com/example/wifisignal/PlayerActivity.java
@@ -47,10 +47,34 @@ public class PlayerActivity extends AppCompatActivity {
         songName=i.getStringExtra("songname");
         txtSname.setText(songName);
 
+        SetRoom t=new SetRoom();
+        t.start();
 
 
+    }
+    class SetRoom extends Thread {
+        @Override
+        public void run() {
+            while(true){
+                try {
 
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
 
+                            if (room != "") {
+                                txtRoom.setText(room);
+                                txtRoom.setVisibility(View.VISIBLE);
+                            }
+                        }
+                    });
+                    Thread.sleep(5000);
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 
